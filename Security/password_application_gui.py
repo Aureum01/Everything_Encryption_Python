4c87efc4790f244d9844cfa60654b89afdead6c4
Aureum01 <71108402+Aureum01@users.noreply.github.com>
2022-12-12 12:27:19 -0500

    Update password_application_gui.py

--- Security/password_application_gui.py
+++ Security/password_application_gui.py
@@ -1,1 +1,37 @@
+import tkinter as tk
+from tkinter import messagebox
+
+# Define the password
+PASSWORD = "secret"
+
+# Create the main window
+root = tk.Tk()
+
+# Define a function to check the password
+def check_password():
+    # Get the password from the input field
+    entered_password = password_entry.get()
+
+    # Check if the entered password is correct
+    if entered_password == PASSWORD:
+        # If the password is correct, show a success message
+        messagebox.showinfo("Success", "The password is correct!")
+    else:
+        # If the password is incorrect, show an error message
+        messagebox.showerror("Error", "The password is incorrect!")
+
+# Create a label for the password prompt
+password_label = tk.Label(root, text="Enter the password:")
+password_label.pack()
+
+# Create an input field for the password
+password_entry = tk.Entry(root, show="*")
+password_entry.pack()
+
+# Create a button to submit the password
+password_button = tk.Button(root, text="Submit", command=check_password)
+password_button.pack()
+
+# Start the main event loop
+root.mainloop()
 
